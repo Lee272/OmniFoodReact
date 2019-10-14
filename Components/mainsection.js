2b74c8dae96b72aaa086c091be81993330bf5a11
Lee <ltritson@gmail.com>
2019-10-14 16:18:37 +0100

    test

---
+++ Components/mainsection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class MainSection extends React.Component {
+
+
+
+  render(){
+
+
+
+
+    return(
+      <div />
+    )
+  }
+}
---
+++ Components/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Navigation extends React.Component {
+
+
+
+
+  render() {
+    return (
+
+      <div className="mainbg" />
+
+    )
+  }
+}
